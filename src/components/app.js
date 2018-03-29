2d3e253addddeab801c0e682bd3210e34b2426c4
Eeps <eeps30@gmail.com>
2018-03-29 14:37:51 -0700

    feature set added, able to add items to list

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 import 'materialize-css/dist/css/materialize.min.css';
 import List from './list';
+import AddForm from './add_form';
+import listData from '../data/todo_items';
 
-const App = () => (
-    <div>
-        <div className="container">
-            <h1 className="center">To Do list</h1>
-            <List/>
-        </div>
-    </div>
-);
+
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            list: listData
+        }
+
+        this.addItem = this.addItem.bind(this)
+    }
+
+    addItem(item){
+        this.setState({
+            list: [...this.state.list, item]
+        })
+    }
+    
+    render(){
+        return(
+            <div>
+                <div className="container">
+                    <h1 className="center">To Do list</h1>
+                    <AddForm add={this.addItem}/>
+                    <List list={this.state.list}/>
+                </div>
+            </div>
+        )
+    }
+    
+};
 
 export default App;
